aad1851633bc8e8852581b1bac95f1d6e671924a
JinTak <jtak23@gmail.com>
2017-12-11 21:15:06 -0700

    working on javascript.

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+// Author: Jin Tak 
+// WDI 6: Project 1 
+// Game Page 
+
+let player1 = {
+    left: 0,
+    bottom: 0
+}
+
+document.onkeyup = function(e){
+    // console.log((document.getElementById('player1').style.bottom));
+   
+    let positionStr = document.getElementById('player1').style.bottom;
+    console.log(positionStr);
+    // let bottom = parseInt(positionStr.slice(0,positionStr.length - 2));
+
+    if(!positionStr === "") {
+        let bottom = parseInt(positionStr.slice(0,positionStr.length - 2));
+    } else {
+        bottom = 0;
+    }
+
+    console.log(bottom);
+
+    if(!bottom > 290) {
+        if(e.keyCode === 65){
+            // console.log('left');
+            player1.left = player1.left - 10;
+            movePlayer();
+        } else if (e.keyCode === 68){
+            // console.log('right');
+            player1.left = player1.left + 10;
+            movePlayer();
+        } else if (e.keyCode === 87){
+            // console.log('up');
+            player1.bottom = player1.bottom + 10;
+            movePlayer();
+        }
+        else if (e.keyCode === 83){
+            // console.log('down');
+            player1.bottom = player1.bottom - 10;
+            movePlayer();
+        }
+    }
+    
+    // if(e.keyCode === 65){
+    //     // console.log('left');
+    //     player1.left = player1.left - 10;
+    //     movePlayer();
+    // } else if (e.keyCode === 68){
+    //     // console.log('right');
+    //     player1.left = player1.left + 10;
+    //     movePlayer();
+    // } else if (e.keyCode === 87){
+    //     // console.log('up');
+    //     player1.bottom = player1.bottom + 10;
+    //     movePlayer();
+    // }
+    // else if (e.keyCode === 83){
+    //     // console.log('down');
+    //     player1.bottom = player1.bottom - 10;
+    //     movePlayer();
+    // }
+
+}
+
+// Function to move the player left, right, up, and down.
+function movePlayer(){
+    let player = document.getElementById('player1');
+    player.style.left = player1.left + "px";
+    player.style.bottom = player1.bottom + "px";
+}
+    
